f6958d790ab6fea74e5c1c5016b4c38f19f28abd
Aditi Karmakar <112576010+AditiShipra@users.noreply.github.com>
2023-07-13 02:01:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ for (var i = 0; i < numberOfDrumButtons; i++) {
 }
 
 //Detecting Keyboard Press
-document.addEventListener("keypress", function(event) {
+document.addEventListener("keydown", function(event) {
 
   makeSound(event.key);
 
